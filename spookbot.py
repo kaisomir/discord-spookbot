57600e897ee5bc550f766cdf73ab458077f8918b
Lawful Lazy <lawful.lazy@gmail.com>
2017-10-05 16:37:57 -0700

    Adding commands are better; add spookmeter command

--- spookbot.py
+++ spookbot.py
@@ -16,11 +16,44 @@ with open("auth.json") as auth:
 
 client = discord.Client()
 
+commands = {}
+
+def command(func):
+    commands[func.__name__] = func
+    return func
+
 @client.event
 async def on_message(message):
-    if message.content[0:5] == '.doot':
-        await client.send_message(message.channel, message.author.mention + ' doot doot')
+    for command in commands.keys():
+        if message.content.startswith('.' + command):
+            logger.info('Command `%s` called from message: %s', command, message.content)
+            await commands[command](message)
 
+# {{{ Commands
+
+@command
+async def doot(message):
+    await client.send_message(message.channel, message.author.mention + ' doot doot')
+
+@command
+async def spookmeter(message):
+    endswith = message.content.lower().endswith
+    async def send(url):
+        await client.send_message(message.channel, url)
+        
+    if endswith('not spooky') or endswith('1'):
+        await send('https://i.imgur.com/OtHOWy4.gif')
+    elif endswith('spoopy') or endswith('2'):
+        await send('https://i.imgur.com/UvoCUa0.gif')
+    elif endswith('p spoopy') or endswith('3'):
+        await send('https://i.imgur.com/HmJXXfh.gif')
+    elif endswith('spooky') or endswith('4'):
+        await send('https://i.imgur.com/o1aLBqG.gif')
+    elif endswith('2spooky') or endswith ('5'):
+        await send('https://i.imgur.com/FToVdJR.gif')
+
+
+# }}}
 
 print("https://discordapp.com/oauth2/authorize?&client_id=365601279713476608&scope=bot&permissions=0")
 client.run(auth['token'])
